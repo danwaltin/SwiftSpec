7b1bc09863c12ded0b5a43a0747ddfc7e2722597
Dan Waltin <dan@waltin.se>
2020-08-07 19:20:35 +0200

    Added a number of tests for expanded scenario names

--- Tests/SwiftSpecTests/UnitTestFileGenerationTests/ExpandedScenariosTests/TestUnitTestsNames.swift
+++ Tests/SwiftSpecTests/UnitTestFileGenerationTests/ExpandedScenariosTests/TestUnitTestsNames.swift
@@ -37,7 +37,171 @@ class TestUnitTestsNames : TestFileGenerationBase {
 			"Name"]
 		)
 	}
+
+	func test_oneScenarioWithoutExamples() {
+		when_parsing(
+			"""
+			Feature: f
+			Scenario Outline: Name
+			""")
+		
+		then_expandedScenarioNamesShouldBe([
+			"Name"]
+		)
+	}
+
+	func test_oneScenarioWithExamplesWithOneTableRow() {
+		when_parsing(
+			"""
+			Feature: f
+			Scenario Outline: Name
+				Examples:
+					| header |
+					| value  |
+			""")
+		
+		then_expandedScenarioNamesShouldBe([
+			"Name_0"]
+		)
+	}
+
+	func test_oneScenarioWithExamplesWithTwoTableRows() {
+		when_parsing(
+			"""
+			Feature: f
+			Scenario Outline: Name
+				Examples:
+					| header |
+					| v1     |
+					| v2     |
+			""")
+		
+		then_expandedScenarioNamesShouldBe([
+			"Name_0",
+			"Name_1"]
+		)
+	}
+
+	func test_oneScenarioWithUnnamedExamplesAndNamedExamlesWithOneTableRowEach() {
+		when_parsing(
+			"""
+			Feature: f
+			Scenario Outline: Name
+				Examples:
+					| header |
+					| v1     |
+
+				Examples: named
+					| header |
+					| v2     |
+			""")
+		
+		then_expandedScenarioNamesShouldBe([
+			"Name_0",
+			"Name_Named_0"]
+		)
+	}
+
+	func test_oneScenarioWithUnnamedExamplesAndNamedExamlesWithTwoAndThreeTableRows() {
+		when_parsing(
+			"""
+			Feature: f
+			Scenario Outline: scenario name
+				Examples:
+					| header |
+					| v1     |
+					| v2     |
+
+				Examples: named
+					| header |
+					| v3     |
+					| v4     |
+					| v5     |
+			""")
+		
+		then_expandedScenarioNamesShouldBe([
+			"ScenarioName_0",
+			"ScenarioName_1",
+			"ScenarioName_named_0",
+			"ScenarioName_named_1",
+			"ScenarioName_named_2"]
+		)
+	}
+
+	func test_oneScenarioWithOneNamedExamlesWithSpaceInNameWithOneTableRow() {
+		when_parsing(
+			"""
+			Feature: f
+			Scenario Outline: Name
+
+				Examples: foo bar
+					| header |
+					| v2     |
+			""")
+		
+		then_expandedScenarioNamesShouldBe([
+			"Name_FooBar_0"]
+		)
+	}
+
+	func test_oneScenarioWithOneNamedExamlesWithDashInNameWithOneTableRow() {
+		when_parsing(
+			"""
+			Feature: f
+			Scenario Outline: Name
+
+				Examples: foo-bar
+					| header |
+					| v2     |
+			""")
+		
+		then_expandedScenarioNamesShouldBe([
+			"Name_FooBar_0"]
+		)
+	}
+
+	func test_oneScenarioWithOneNamedExamlesWithSwedishCharactersInNameWithOneTableRow() {
+		when_parsing(
+			"""
+			Feature: f
+			Scenario Outline: Name
+
+				Examples: Xå Xä Xö Åx Äx Öx
+					| header |
+					| v2     |
+			""")
+		
+		then_expandedScenarioNamesShouldBe([
+			"Name_XaXaXoAxAxOx_0"]
+		)
+	}
 	
+	func test_oneScenarioWithTwoNamedExamlesWithTwoAndThreeTableRows() {
+		when_parsing(
+			"""
+			Feature: f
+			Scenario Outline: scenario name
+				Examples: One
+					| header |
+					| v1     |
+					| v2     |
+
+				Examples: Two
+					| header |
+					| v3     |
+					| v4     |
+					| v5     |
+			""")
+		
+		then_expandedScenarioNamesShouldBe([
+			"ScenarioName_One_0",
+			"ScenarioName_One_1",
+			"ScenarioName_Two_0",
+			"ScenarioName_Two_1",
+			"ScenarioName_Two_2"]
+		)
+	}
+
 	// MARK: - Givens whens and thens
 	private func then_expandedScenarioNamesShouldBe(_ expected: [String], file: StaticString = #file, line: UInt = #line) {
 		guard let feature = feature() else {
